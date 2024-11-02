d2987f5e3cb13d3663a36c6e65c123ec9d7ece31
Furkan Yılmaz <97380395+Furkyleon@users.noreply.github.com>
2024-11-02 20:53:53 +0300

    Delete ecommerce directory

--- ecommerce/store/serializers.py
+++
@@ -1,8 +0,0 @@
-# accounts/serializers.py
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '_all_'  # Alternatively, specify fields as a list, e.g., ['productID', 'name', 'price']
