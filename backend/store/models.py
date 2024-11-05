c280f038a206b2474ff49614f09055c4110062d4
Ilgın Simay Özcan <ilgin.ozcan@sabanciuniv.edu>
2024-11-05 18:02:59 +0300

    shipping address model is added

--- backend/store/models.py
+++ backend/store/models.py
@@ -68,3 +68,15 @@ class Product(models.Model):
         
     def _str_(self):
         return self.name
+
+class ShippingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	state = models.CharField(max_length=200, null=False)
+	zipcode = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address   
